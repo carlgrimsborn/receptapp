96c84f3510e2c3070782c14962c25ab71e90e25f
Carl Grimsborn <carlgrimsborn@hotmail.com>
2017-09-08 14:07:37 +0200

    Initial Commit

---
+++ ReceptApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReceptApp
+//
+//  Created by Carl Grimsborn on 2017-09-08.
+//  Copyright © 2017 Carl Grimsborn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
