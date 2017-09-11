f03f6c1f2e469d404a059120b175b16ef081a579
Carl Grimsborn <carlgrimsborn@hotmail.com>
2017-09-11 16:21:05 +0200

    first commit

---
+++ ReceptApp/AddVC.swift
@@ -0,0 +1,25 @@
+//
+//  AddVC.swift
+//  ReceptApp
+//
+//  Created by Carl Grimsborn on 2017-09-11.
+//  Copyright © 2017 Carl Grimsborn. All rights reserved.
+//
+
+import UIKit
+
+class AddVC: UIViewController {
+
+    @IBOutlet weak var addTitleTxtEdit: UITextField!
+    
+    
+    @IBOutlet weak var addDescriptionTxtEdit: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
---
+++ ReceptApp/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReceptApp
+//
+//  Created by Carl Grimsborn on 2017-09-08.
+//  Copyright © 2017 Carl Grimsborn. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    @IBOutlet weak var addRecipeBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+
+}
+
