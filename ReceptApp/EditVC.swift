792177fba01c8494e25dc02d8496588dbb47aa0a
Carl Grimsborn <carlgrimsborn@hotmail.com>
2017-09-13 15:46:51 +0200

    added classes

---
+++ ReceptApp/EditVC.swift
@@ -0,0 +1,30 @@
+//
+//  EditVC.swift
+//  ReceptApp
+//
+//  Created by Carl Grimsborn on 2017-09-13.
+//  Copyright © 2017 Carl Grimsborn. All rights reserved.
+//
+
+import UIKit
+
+class EditVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
